87acab0f4173d6978df365e845e2dcdd2300cea9
Talon Daniels <talondaniels@gmail.com>
2013-01-27 13:28:40 -0700

    added homework 2 project

---
+++ homework2/homework2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  homework2
+//
+//  Created by Talon Daniels on 1/27/13.
+//  Copyright (c) 2013 Talon Daniels. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
