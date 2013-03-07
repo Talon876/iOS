4e71d302a847c8f5a1899f1bd656a3654fce15ba
John Bender <john@generalui.com>
2013-03-07 14:49:55 -0800

    grading for assignment 3

--- homework3/homework3/HTMasterViewController.m
+++ homework3/homework3/HTMasterViewController.m
@@ -6,6 +6,8 @@
 //  Copyright (c) 2013 Talon Daniels. All rights reserved.
 //
 
+// Good work, 100%. The code could use a little cleanup...
+
 #import "HTMasterViewController.h"
 
 #import "HTDetailViewController.h"
