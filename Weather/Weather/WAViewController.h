31cc811e2cc2820d9ba3f34e70e35cdcff2b403f
Talon Daniels <talondaniels@gmail.com>
2013-03-25 18:57:35 -0600

    added Wthr project

---
+++ Weather/Weather/WAViewController.h
@@ -0,0 +1,13 @@
+//
+//  WAViewController.h
+//  Weather
+//
+//  Created by Talon Daniels on 3/25/13.
+//  Copyright (c) 2013 Talon Daniels. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WAViewController : UIViewController
+
+@end
---
+++ Weather/Weather/WAViewController.m
@@ -0,0 +1,29 @@
+//
+//  WAViewController.m
+//  Weather
+//
+//  Created by Talon Daniels on 3/25/13.
+//  Copyright (c) 2013 Talon Daniels. All rights reserved.
+//
+
+#import "WAViewController.h"
+
+@interface WAViewController ()
+
+@end
+
+@implementation WAViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
